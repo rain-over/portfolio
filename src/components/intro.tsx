f935f253b3581b8933b07ddb219ceace2df514ce
rain <rainbangles90@gmail.com>
2023-10-08 17:36:03 +1300

    intro transitions

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -24,19 +24,20 @@ import {
 import { FiDownload, FiMail } from 'react-icons/fi';
 
 import { useSectionInView } from '@/../lib/hooks';
-import profile from '@/../public/profile.png';
+import profile from '@/../public/profile_photo.png';
 import Link from 'next/link';
 import { Radley } from 'next/font/google';
 import { useEffect, useState } from 'react';
 import { blob } from 'stream/consumers';
+import { Console } from 'console';
+
+type JustifyContentProperty = 'center' | 'space-around';
 
 const transition: Transition = {
-  type: 'tween',
-  ease: 'easeIn',
-  // duration: 5,
-  // stiffness: 50,
-  // damping: 20,
-  // mass: 0.8,
+  type: 'spring',
+  // stiffness: 500,
+  damping: 30,
+  duration: 3,
 };
 
 const flexDirectionValues: React.CSSProperties['flexDirection'][] = [
@@ -45,6 +46,11 @@ const flexDirectionValues: React.CSSProperties['flexDirection'][] = [
   'row-reverse',
   'column-reverse',
 ];
+
+// const justifyContentValues: JustifyContentProperty = [
+//   'center',
+//   'space-between',
+// ];
 
 const radley = Radley({ subsets: ['latin'], weight: '400' });
 const radley_i = Radley({
@@ -69,14 +75,19 @@ export default function Intro() {
   const [flexDirection, setFlexDirection] = useState<
     React.CSSProperties['flexDirection']
   >(flexDirectionValues[0]);
+  const [justifyContent, setJustifyContent] =
+    useState<JustifyContentProperty>('center');
   const [width, setWidth] = useState('');
 
   const change = () => {
-    setFlexDirection(flexDirectionValues[1]);
-    setWidth('1200px');
+    console.log('change');
+    setJustifyContent('space-around');
+    // setFlexDirection(flexDirectionValues[1]);
+    // setWidth('1200px');
   };
+
   const { ref } = useSectionInView('Home');
-  const name = "I'm Rain";
+  const name: Array<String> = "I'm Rain   ".split('');
 
   const x = useMotionValue(200);
   const y = useMotionValue(200);
@@ -115,40 +126,40 @@ export default function Intro() {
         transition={{ delay: 1.2, duration: 2, ease: 'easeOut' }}
       ></motion.div>
       <motion.div
-        className="items-center flex justify-between"
-        layout
-        transition={transition}
-        style={{ flexDirection, width }}
+        className="absolute"
+        animate={{
+          opacity: 0.1,
+          // transitionEnd: { display: 'none' },
+        }}
+        transition={{ delay: 1, duration: 0.8 }}
+        // onAnimationComplete={() => change()}
       >
         <motion.div
+          className="text-[5rem] z-[1] origin-bottom-right rotate-[15deg]"
           animate={{
-            opacity: 0,
-            transitionEnd: { display: 'none' },
+            rotate: [15, 10, 15, 5],
           }}
-          transition={{ delay: 4 }}
-          onAnimationComplete={() => change()}
+          transition={{ duration: 0.8 }}
         >
-          <motion.div
-            className="text-[5rem] z-[1] origin-bottom-right rotate-[15deg]"
-            animate={{
-              rotate: [15, 10, 15, 5],
-            }}
-            transition={{ duration: 0.8 }}
-          >
-            <span>👋</span>
-          </motion.div>
+          <span>👋</span>
         </motion.div>
-        <div>
+      </motion.div>
+      <motion.div
+        className="intro-content items-center flex flex-row w-[1200px]"
+        style={{ justifyContent }}
+      >
+        <motion.div layout transition={transition}>
           <motion.div
             // className="text-[5rem]"
             initial={{ fontSize: '5rem' }}
             animate={{ fontSize: '2rem' }}
-            transition={{ delay: 5, duration: 1 }}
+            transition={{ delay: 3.3, duration: 1 }}
+            // onAnimationComplete={() => change()}
           >
             <span className={`${radley_i.className}`}>Hello</span>,{' '}
             <span className="sr-only">I'm Rain</span>
             <span aria-hidden>
-              {name.split('').map((c, i) => (
+              {name.map((c, i) => (
                 <motion.span
                   className={`${radley.className} inline-block whitespace-pre`}
                   key={i}
@@ -164,53 +175,67 @@ export default function Intro() {
                     delay: 2.2 + (i < 3 ? 0 : i) / 10,
                     duration: 0.01,
                   }}
+                  onAnimationComplete={() => i === name.length - 1 && change()}
                 >
                   {c}
                 </motion.span>
               ))}
             </span>
           </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 4.5 }}
-          >
+          <motion.div>
             <p>
-              <span className="font-bold text-[3rem]">Full Stack Engineer</span>{' '}
+              <motion.span
+                className="inline-block font-bold text-[3rem]"
+                initial={{ opacity: 0, y: 150 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ ...transition, delay: 4 }}
+              >
+                Full Stack Developer
+              </motion.span>{' '}
               <br />
-              I've been doing web development for nearly a{' '}
-              <span className="font-bold">decade</span>. <br />I have built web
-              apps with multiple tech stacks and right now, <br />
-              I'm focused on{' '}
-              <span className="font-medium ">React (Next.js)</span> and{' '}
-              <span className="font-medium ">.NET Core</span>.
+              <motion.span
+                className="inline-block"
+                initial={{ opacity: 0, y: 150 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ ...transition, delay: 5 }}
+              >
+                I've been doing web development for nearly a{' '}
+                <span className="font-medium">decade</span>. <br />I have built
+                web apps with multiple tech stacks and right now, <br />
+                my focused on <span className="font-medium ">
+                  .NET Core
+                </span>{' '}
+                and <span className="font-medium ">React (Next.js)</span>.
+              </motion.span>
             </p>
           </motion.div>
-        </div>
+        </motion.div>
         <motion.div
-        // style={{
-        //   width: 400,
-        //   height: 400,
-        //   display: 'flex',
-        //   placeItems: 'center',
-        //   placeContent: 'center',
-        //   borderRadius: 30,
-        //   backgroundColor: 'rgba(255, 255, 255, 0.05)',
-        //   perspective: 400,
-        // }}
-        // onMouseOver={handleMouse}
+          className="overflow-hidden text-center"
+          initial={{ width: 0 }}
+          animate={{
+            width: '300px',
+          }}
+          transition={{ delay: 3.2 }}
         >
-          <motion.div
-            initial={{ opacity: 1 }}
-            // style={{
-            //   width: 150,
-            //   height: 150,
-            //   borderRadius: 30,
-            //   backgroundColor: '#fff',
-            //   rotateX,
-            //   rotateY,
-            // }}
+          <motion.span
+            className="circle-face"
+            style={{}}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 3.5, duration: 2 }}
           >
+            <motion.span
+              initial={{ opacity: 0, y: -100 }}
+              style={{
+                backgroundImage: `url('@/../profile_photo.png')`,
+              }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 5, duration: 1 }}
+              // style="background-image:url(https://i.stack.imgur.com/bdZeE.png);"
+            ></motion.span>
+          </motion.span>
+          <motion.div>
             {/* <Image
               alt="profile picture"
               className="border-4 border-black border-opacity-5
