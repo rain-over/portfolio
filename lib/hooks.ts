aba8f8c168d302c010aca2f0cd9a514d878e6f2e
rain <rainbangles90@gmail.com>
2023-10-15 15:24:15 +1300

    test condition

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -21,8 +21,10 @@ export function useSectionInView(section: SectionName, threshold = 0.65) {
 
 export const useMotions = () => {
   let timestamp = 1000;
-  if (localStorage !== undefined) {
+  try {
     timestamp = JSON.parse(localStorage?.getItem('timestamp') || '1000');
+  } catch (error) {
+    console.log('useMotions', error);
   }
   const currTimestamp = Date.now();
 
