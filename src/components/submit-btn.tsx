02b254b7ade6f16ecdd7c539fcef1fb19a680335
rain <rainbangles90@gmail.com>
2023-11-30 08:02:38 +1300

    remove proxy

--- src/components/submit-btn.tsx
+++ src/components/submit-btn.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaPaperPlane } from 'react-icons/fa';
+//@ts-expect-error
 import { experimental_useFormStatus as useFormStatus } from 'react-dom';
 
 export default function SubmitBtn() {
@@ -8,7 +9,7 @@ export default function SubmitBtn() {
   return (
     <button
       type="submit"
-      className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 bg-opacity-75 disabled:scale-100 disabled:bg-opacity-65"
+      className="disabled:bg-opacity-65 group flex h-[3rem] w-[8rem] items-center justify-center gap-2 rounded-full bg-gray-900 bg-opacity-75 text-white outline-none transition-all hover:scale-110 hover:bg-gray-950 focus:scale-110 active:scale-105 disabled:scale-100"
       disabled={pending}
     >
       {pending ? (
