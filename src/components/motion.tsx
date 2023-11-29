9d05018acdde620a90f678ac3fe92f5c04a77be5
rain <rainbangles90@gmail.com>
2023-11-30 07:50:07 +1300

    remove proxy

--- src/components/motion.tsx
+++ src/components/motion.tsx
@@ -3,25 +3,26 @@ import { motion } from 'framer-motion';
 
 const prox = new Proxy(
   {},
-  {
-    get: (_, tagName) => {
-      return ({ children, ...props }) => {
-        console.log(process.env.DISABLE_MOTION);
-        if (true) {
-          if (props?.transition?.delay) {
-            props.transition.delay = 0;
-          }
-          if (props?.transition?.duration) {
-            props.transition.duration = 0;
-          }
+  {}
+  // {
+  //   get: (_, tagName) => {
+  //     return ({ children, ...props }) => {
+  //       console.log(process.env.DISABLE_MOTION);
+  //       if (true) {
+  //         if (props?.transition?.delay) {
+  //           props.transition.delay = 0;
+  //         }
+  //         if (props?.transition?.duration) {
+  //           props.transition.duration = 0;
+  //         }
 
-          return createElement(tagName, props, children);
-        }
+  //         return createElement(tagName, props, children);
+  //       }
 
-        return createElement(motion[tagName], props, children);
-      };
-    },
-  }
+  //       return createElement(motion[tagName], props, children);
+  //     };
+  //   },
+  // }
 );
 
 export default prox;
